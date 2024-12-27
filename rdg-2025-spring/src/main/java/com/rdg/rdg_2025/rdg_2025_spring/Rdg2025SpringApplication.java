bf7ff0f1d4e87c0658b4a1ed16bd8e7c3f82d365
Julian Humble <humblejulian@gmail.com>
2024-12-27 11:17:23 +0000

    initial commit

---
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/Rdg2025SpringApplication.java
@@ -0,0 +1,13 @@
+package com.rdg.rdg_2025.rdg_2025_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rdg2025SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rdg2025SpringApplication.class, args);
+	}
+
+}
