9a77d616fc6dfe7d3b860482db382167e4bcfe3c
Julian Humble <humblejulian@gmail.com>
2025-01-06 15:44:42 +0000

    added auth controllers

---
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/controllers/AuthController.java
@@ -0,0 +1,138 @@
+package com.rdg.rdg_2025.rdg_2025_spring.controllers;
+
+import com.rdg.rdg_2025.rdg_2025_spring.models.ERole;
+import com.rdg.rdg_2025.rdg_2025_spring.models.Role;
+import com.rdg.rdg_2025.rdg_2025_spring.models.User;
+import com.rdg.rdg_2025.rdg_2025_spring.payload.request.LoginRequest;
+import com.rdg.rdg_2025.rdg_2025_spring.payload.request.SignUpRequest;
+import com.rdg.rdg_2025.rdg_2025_spring.payload.response.JwtResponse;
+import com.rdg.rdg_2025.rdg_2025_spring.payload.response.MessageResponse;
+import com.rdg.rdg_2025.rdg_2025_spring.repository.RoleRepository;
+import com.rdg.rdg_2025.rdg_2025_spring.repository.UserRepository;
+import com.rdg.rdg_2025.rdg_2025_spring.security.jwt.JwtUtils;
+import com.rdg.rdg_2025.rdg_2025_spring.security.services.UserDetailsImpl;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
+        );
+
+        System.out.println("created authentication token");
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(
+                jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles
+        ));
+    }
+
+
+    @PostMapping("signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        User user = new User(
+                signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        System.out.println(user);
+        System.out.println(strRoles);
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found"));
+                        roles.add(adminRole);
+
+                        break;
+
+                    default:
+                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error : Role is not found"));
+                        roles.add(userRole);
+                }
+
+
+            });
+        }
+
+        System.out.println(roles);
+
+        user.setRoles(roles);
+
+        System.out.println(user);
+        user.getRoles().forEach(role -> System.out.println("Role ID: " + role.getId() + ", Role Name: " + role.getName()));
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully"));
+
+    }
+}
--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/security/jwt/JwtUtils.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/security/jwt/JwtUtils.java
@@ -18,10 +18,10 @@ public class JwtUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-    @Value("${bezkoder.app.jwtSecret}")
+    @Value("${rdg.app.jwtSecret}")
     private String jwtSecret;
 
-    @Value("${bezkoder.app.jwtExpirationMs}")
+    @Value("${rdg.app.jwtExpirationMs}")
     private int jwtExpirationMs;
 
     public String generateJwtToken(Authentication authentication) {
