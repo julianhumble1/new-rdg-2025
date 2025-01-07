8799c2e0d746eee2d19ea1953613dec67f4d0489
Julian Humble <humblejulian@gmail.com>
2025-01-07 10:59:18 +0000

    installed dev tools for live reload and renamed auth controller

--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/controllers/CheckAuthController.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/controllers/CheckAuthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/checkAuth")
-public class TestAuthController {
+public class CheckAuthController {
 
     @GetMapping("/all")
     public String allAccess() {
