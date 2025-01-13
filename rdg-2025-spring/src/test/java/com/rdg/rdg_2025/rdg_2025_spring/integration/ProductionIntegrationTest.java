45219fb68c6d1ffcada941df99ff0a74047adfb6
Julian Humble <humblejulian@gmail.com>
2025-01-13 18:43:10 +0000

    add new production integration test 10 passed

--- rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/integration/ProductionIntegrationTest.java
+++ rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/integration/ProductionIntegrationTest.java
@@ -229,5 +229,18 @@ public class ProductionIntegrationTest {
                 );
     }
 
+    @Test
+    void testSundownersNotBooleanResponds400BadRequest() throws Exception {
+        mockMvc.perform(post("/productions/new")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", adminToken)
+                        .content(
+                                "{ \"name\": \"Test Production\", \"venueId\": " + testVenue1.getId() + ", \"author\": \"Test Author\", \"description\": \"Test Description\", " +
+                                        "\"auditionDate\": \"2025-10-10T10:00:00\", \"sundowners\": \"Bad Boolean\", \"notConfirmed\": false, \"flyerFile\": \"Test Flyer File\" }"
+                        ))
+                .andExpect(status().isBadRequest()
+                );
+    }
+
 
 }
