c7fe513cf3d5d7b329d3ed4348b4e73e4d0867b7
Julian Humble <humblejulian@gmail.com>
2025-01-13 18:52:41 +0000

    add new production integration test 14 passed

--- rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/integration/ProductionIntegrationTest.java
+++ rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/integration/ProductionIntegrationTest.java
@@ -280,5 +280,18 @@ public class ProductionIntegrationTest {
                 );
     }
 
+    @Test
+    void testUserTokenResponds403() throws Exception {
+        mockMvc.perform(post("/productions/new")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", userToken)
+                        .content(
+                                "{ \"name\": \"Test Production\", \"venueId\": " + testVenue1.getId() + ", \"author\": \"Test Author\", \"description\": \"Test Description\", " +
+                                        "\"auditionDate\": \"2025-10-10T10:00:00\", \"sundowners\": false, \"notConfirmed\": false, \"flyerFile\": \"Test Flyer File\" }"
+                        ))
+                .andExpect(status().isForbidden()
+                );
+    }
+
 
 }
