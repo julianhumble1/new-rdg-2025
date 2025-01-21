1f1f5d2623818c2099161baaebab3054e93e63f2
Julian Humble <humblejulian@gmail.com>
2025-01-21 15:26:31 +0000

    auth sign in integration test 3 passed

--- rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/integration/AuthIntegrationTest.java
+++ rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/integration/AuthIntegrationTest.java
@@ -73,6 +73,17 @@ public class AuthIntegrationTest {
                     .andExpect(status().isBadRequest());
 
         }
+
+        @Test
+        void testRequestMissingUsernameResponds400() throws Exception {
+            // Arrange
+            // Act
+            mockMvc.perform(post("/auth/signin")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("  \"password\": \"password123\" "))
+                    .andExpect(status().isBadRequest());
+
+        }
     }
 
 
