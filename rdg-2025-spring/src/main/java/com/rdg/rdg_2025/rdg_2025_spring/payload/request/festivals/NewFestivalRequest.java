ebb73c117a0fd65f0c51036a45682d313fa821cd
Julian Humble <humblejulian@gmail.com>
2025-01-22 10:56:43 +0000

    updated NewFestivalRequest object to lombok

--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/request/festivals/NewFestivalRequest.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/request/festivals/NewFestivalRequest.java
@@ -3,8 +3,12 @@ package com.rdg.rdg_2025.rdg_2025_spring.payload.request.festivals;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+
+@Getter @Setter @NoArgsConstructor
 public class NewFestivalRequest {
 
     @NotBlank
@@ -29,9 +33,6 @@ public class NewFestivalRequest {
         this.description = description;
     }
 
-    public NewFestivalRequest() {
-    }
-
     @Override
     public String toString() {
         return "NewFestivalRequest{" +
@@ -41,45 +42,5 @@ public class NewFestivalRequest {
                 ", month=" + month +
                 ", description='" + description + '\'' +
                 '}';
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVenueId() {
-        return venueId;
-    }
-
-    public void setVenueId(int venueId) {
-        this.venueId = venueId;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public int getMonth() {
-        return month;
-    }
-
-    public void setMonth(int month) {
-        this.month = month;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
