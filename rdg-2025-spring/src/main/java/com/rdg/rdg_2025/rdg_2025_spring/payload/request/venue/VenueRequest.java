752a6fc073ea0f6be108e5567397a77eb59907fa
Julian Humble <humblejulian@gmail.com>
2025-01-22 11:00:28 +0000

    updated VenueRequest object to lombok

--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/request/venue/VenueRequest.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/request/venue/VenueRequest.java
@@ -1,7 +1,11 @@
 package com.rdg.rdg_2025.rdg_2025_spring.payload.request.venue;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter @Setter @NoArgsConstructor
 public class VenueRequest {
 
     @NotBlank
@@ -13,9 +17,6 @@ public class VenueRequest {
     private String town;
     private String url;
 
-    public VenueRequest() {
-    }
-
     public VenueRequest(String name, String notes, String postcode, String address, String town, String url) {
         this.name = name;
         this.notes = notes;
@@ -23,65 +24,5 @@ public class VenueRequest {
         this.address = address;
         this.town = town;
         this.url = url;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
-
-    public String getPostcode() {
-        return postcode;
-    }
-
-    public void setPostcode(String postcode) {
-        this.postcode = postcode;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getTown() {
-        return town;
-    }
-
-    public void setTown(String town) {
-        this.town = town;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    @Override
-    public String toString() {
-        return "VenueRequest{" +
-                "name='" + name + '\'' +
-                ", notes='" + notes + '\'' +
-                ", postcode='" + postcode + '\'' +
-                ", address='" + address + '\'' +
-                ", town='" + town + '\'' +
-                ", url='" + url + '\'' +
-                '}';
     }
 }
