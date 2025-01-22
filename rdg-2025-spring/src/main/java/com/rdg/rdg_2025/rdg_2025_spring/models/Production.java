6b2d0df4d223b7781a5a83ff7906b1476b11ad19
Julian Humble <humblejulian@gmail.com>
2025-01-22 10:42:22 +0000

    updated Production object to lombok

--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/models/Production.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/models/Production.java
@@ -3,6 +3,9 @@ package com.rdg.rdg_2025.rdg_2025_spring.models;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.rdg.rdg_2025.rdg_2025_spring.helpers.SlugUtils;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
@@ -10,6 +13,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name="productions")
+@Getter @Setter @NoArgsConstructor
 public class Production {
 
     @Id
@@ -38,9 +42,6 @@ public class Production {
     @Column(nullable = false)
     private String slug;
 
-    public Production() {
-    }
-
     public Production(String name, Venue venue, String author, String description, LocalDateTime auditionDate, boolean sundowners, boolean notConfirmed, String flyerFile) {
         this.name = name;
         this.venue = venue;
@@ -55,101 +56,5 @@ public class Production {
         this.updatedAt = LocalDateTime.now();
 
         this.slug = SlugUtils.generateSlug(name);
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Venue getVenue() {
-        return venue;
-    }
-
-    public void setVenue(Venue venue) {
-        this.venue = venue;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getAuditionDate() {
-        return auditionDate;
-    }
-
-    public void setAuditionDate(LocalDateTime auditionDate) {
-        this.auditionDate = auditionDate;
-    }
-
-    public boolean isSundowners() {
-        return sundowners;
-    }
-
-    public void setSundowners(boolean sundowners) {
-        this.sundowners = sundowners;
-    }
-
-    public boolean isNotConfirmed() {
-        return notConfirmed;
-    }
-
-    public void setNotConfirmed(boolean notConfirmed) {
-        this.notConfirmed = notConfirmed;
-    }
-
-    public String getFlyerFile() {
-        return flyerFile;
-    }
-
-    public void setFlyerFile(String flyerFile) {
-        this.flyerFile = flyerFile;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public String getSlug() {
-        return slug;
-    }
-
-    public void setSlug(String slug) {
-        this.slug = slug;
     }
 }
