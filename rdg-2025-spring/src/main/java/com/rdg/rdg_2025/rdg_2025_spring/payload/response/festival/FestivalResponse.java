8dca676dcfabad4a59955aa228a1d4aa316b81a5
Julian Humble <humblejulian@gmail.com>
2025-01-22 11:05:50 +0000

    updated festvial response objects to lombok

--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/response/festival/FestivalResponse.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/response/festival/FestivalResponse.java
@@ -1,20 +1,15 @@
 package com.rdg.rdg_2025.rdg_2025_spring.payload.response.festival;
 
 import com.rdg.rdg_2025.rdg_2025_spring.models.Festival;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
 public class FestivalResponse {
 
     private Festival festival;
 
     public FestivalResponse(Festival festival) {
-        this.festival = festival;
-    }
-
-    public Festival getFestival() {
-        return festival;
-    }
-
-    public void setFestival(Festival festival) {
         this.festival = festival;
     }
 }
--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/response/festival/FestivalsResponse.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/response/festival/FestivalsResponse.java
@@ -1,22 +1,17 @@
 package com.rdg.rdg_2025.rdg_2025_spring.payload.response.festival;
 
 import com.rdg.rdg_2025.rdg_2025_spring.models.Festival;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 
+@Getter @Setter
 public class FestivalsResponse {
 
     private ArrayList<Festival> festivals;
 
     public FestivalsResponse(ArrayList<Festival> festivals) {
-        this.festivals = festivals;
-    }
-
-    public ArrayList<Festival> getFestivals() {
-        return festivals;
-    }
-
-    public void setFestivals(ArrayList<Festival> festivals) {
         this.festivals = festivals;
     }
 }
