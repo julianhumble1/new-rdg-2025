eef3c97a9579abe109305a4bd53845c4036cff03
Julian Humble <humblejulian@gmail.com>
2025-01-22 10:58:40 +0000

    updated NewProductionRequest object to lombok

--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/request/production/NewProductionRequest.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/request/production/NewProductionRequest.java
@@ -1,9 +1,13 @@
 package com.rdg.rdg_2025.rdg_2025_spring.payload.request.production;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Getter @Setter @NoArgsConstructor
 public class NewProductionRequest {
 
     @NotBlank
@@ -17,9 +21,6 @@ public class NewProductionRequest {
     private boolean notConfirmed;
     private String flyerFile;
 
-    public NewProductionRequest() {
-    }
-
     public NewProductionRequest(String name, int venueId, String author, String description, LocalDateTime auditionDate, boolean sundowners, boolean notConfirmed, String flyerFile) {
         this.name = name;
         this.venueId = venueId;
@@ -28,70 +29,6 @@ public class NewProductionRequest {
         this.auditionDate = auditionDate;
         this.sundowners = sundowners;
         this.notConfirmed = notConfirmed;
-        this.flyerFile = flyerFile;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVenueId() {
-        return venueId;
-    }
-
-    public void setVenueId(int venueId) {
-        this.venueId = venueId;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getAuditionDate() {
-        return auditionDate;
-    }
-
-    public void setAuditionDate(LocalDateTime auditionDate) {
-        this.auditionDate = auditionDate;
-    }
-
-    public boolean isSundowners() {
-        return sundowners;
-    }
-
-    public void setSundowners(boolean sundowners) {
-        this.sundowners = sundowners;
-    }
-
-    public boolean isNotConfirmed() {
-        return notConfirmed;
-    }
-
-    public void setNotConfirmed(boolean notConfirmed) {
-        this.notConfirmed = notConfirmed;
-    }
-
-    public String getFlyerFile() {
-        return flyerFile;
-    }
-
-    public void setFlyerFile(String flyerFile) {
         this.flyerFile = flyerFile;
     }
 }
