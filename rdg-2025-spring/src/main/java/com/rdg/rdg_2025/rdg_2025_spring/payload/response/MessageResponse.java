8a1ba87c264f898e7b5ed6f5ade94e202d2f7e4d
Julian Humble <humblejulian@gmail.com>
2025-01-22 11:11:18 +0000

    updated message response object to lombok

--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/response/MessageResponse.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/response/MessageResponse.java
@@ -1,18 +1,15 @@
 package com.rdg.rdg_2025.rdg_2025_spring.payload.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MessageResponse {
 
     private String message;
 
     public MessageResponse(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
         this.message = message;
     }
 }
