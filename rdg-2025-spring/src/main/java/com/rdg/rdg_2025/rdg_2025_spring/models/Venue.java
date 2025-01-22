a45bbc7db511dab58068dc7e44ee66ead775d658
Julian Humble <humblejulian@gmail.com>
2025-01-22 10:34:35 +0000

    updated Venue object to lombok

--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/models/Venue.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/models/Venue.java
@@ -3,6 +3,9 @@ package com.rdg.rdg_2025.rdg_2025_spring.models;
 import com.fasterxml.jackson.annotation.*;
 import com.rdg.rdg_2025.rdg_2025_spring.helpers.SlugUtils;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
@@ -11,6 +14,7 @@ import java.util.List;
 
 @Entity
 @Table(name="venues")
+@Getter @Setter @NoArgsConstructor
 public class Venue {
 
     @Id
@@ -72,104 +76,5 @@ public class Venue {
                 ", productions=" + productions +
                 ", festivals=" + festivals +
                 '}';
-    }
-
-    public Venue() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
-
-    public String getPostcode() {
-        return postcode;
-    }
-
-    public void setPostcode(String postcode) {
-        this.postcode = postcode;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getTown() {
-        return town;
-    }
-
-    public void setTown(String town) {
-        this.town = town;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public String getSlug() {
-        return slug;
-    }
-
-    public void setSlug(String slug) {
-        this.slug = slug;
-    }
-
-    public List<Production> getProductions() {
-        return productions;
-    }
-
-    public void setProductions(List<Production> productions) {
-        this.productions = productions;
-    }
-
-    public List<Festival> getFestivals() {
-        return festivals;
-    }
-
-    public void setFestivals(List<Festival> festivals) {
-        this.festivals = festivals;
     }
 }
