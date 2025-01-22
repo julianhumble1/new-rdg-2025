41769e13f0622748cf815e1be4eaa8b27af43915
Julian Humble <humblejulian@gmail.com>
2025-01-22 10:39:40 +0000

    updated Role object to lombok

--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/models/Role.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/models/Role.java
@@ -1,9 +1,13 @@
 package com.rdg.rdg_2025.rdg_2025_spring.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name="roles")
+@Getter @Setter @NoArgsConstructor
 public class Role {
 
     @Id
@@ -14,26 +18,4 @@ public class Role {
     @Column(length = 20)
     private ERole name;
 
-    public Role() {
-    }
-
-    public Role(ERole name) {
-        this.name = name;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public ERole getName() {
-        return name;
-    }
-
-    public void setName(ERole name) {
-        this.name = name;
-    }
 }
