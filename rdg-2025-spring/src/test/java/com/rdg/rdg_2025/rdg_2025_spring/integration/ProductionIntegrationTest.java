f99cde848e0da56b4b982a571b4e6910fd04bf25
Julian Humble <humblejulian@gmail.com>
2025-01-27 15:20:03 +0000

    update production integration test 9 passed

--- rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/integration/ProductionIntegrationTest.java
+++ rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/integration/ProductionIntegrationTest.java
@@ -575,5 +575,20 @@ public class ProductionIntegrationTest {
                     .andExpect(status().isNotFound());
         }
 
+        @Test
+        void testVenueIdNotIntegerResponds400() throws Exception {
+            // Arrange
+            Venue managedTestVenue2 = venueRepository.findById(testVenue2.getId()).orElseThrow(() -> new RuntimeException("Venue not found"));
+            // Act & Assert
+            mockMvc.perform(patch("/productions/" + testExistingProduction.getId())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .header("Authorization", adminToken)
+                            .content(
+                                    "{ \"name\": \"Updated Test Production\", \"venueId\": " + "\"not an integer\"" +  ", \"author\": \"Updated Test Author\", \"description\": \"Updated Test Description\", " +
+                                            "\"auditionDate\": \"2025-11-10T10:00:00\", \"sundowners\": true, \"notConfirmed\": true, \"flyerFile\": \"Updated Test Flyer File\" }"
+                            ))
+                    .andExpect(status().isBadRequest());
+        }
+
     }
 }
