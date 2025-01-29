e0f5c0479e3b96984ccfc4ca7476edea8cf731da
Julian Humble <humblejulian@gmail.com>
2025-01-29 09:33:10 +0000

    fixed production count method to be case sensitive

--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/repository/ProductionRepository.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/repository/ProductionRepository.java
@@ -2,11 +2,14 @@ package com.rdg.rdg_2025.rdg_2025_spring.repository;
 
 import com.rdg.rdg_2025.rdg_2025_spring.models.Production;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductionRepository extends JpaRepository<Production, Integer> {
 
-    int countByNameStartingWith(String prefix);
+    @Query(value = "SELECT COUNT(*) FROM Productions p WHERE p.name LIKE BINARY :prefix%", nativeQuery = true)
+    int countByNameStartingWith(@Param("prefix") String prefix);
 
 }
