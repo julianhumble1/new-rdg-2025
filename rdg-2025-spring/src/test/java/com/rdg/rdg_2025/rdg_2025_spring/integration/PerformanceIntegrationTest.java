a2115ed68fe820878b13692eb1b44c4b9ee3a64c
Julian Humble <humblejulian@gmail.com>
2025-01-31 13:49:16 +0000

    add new performance integration test 13 passed

--- rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/integration/PerformanceIntegrationTest.java
+++ rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/integration/PerformanceIntegrationTest.java
@@ -444,5 +444,27 @@ public class PerformanceIntegrationTest {
 
         }
 
+        @Test
+        void testVenueIdBlankResponds400() throws Exception {
+            // Act & Assert
+            mockMvc.perform(post("/performances")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .header("Authorization", adminToken)
+                            .content(
+                                    "{" +
+                                            "\"productionId\": " + testProductionId +  ", " +
+                                            "\"venueId\": \"\", " +
+                                            "\"festivalId\": " + testFestivalId + ", " +
+                                            "\"time\": \"2025-10-10T10:00:00\", " +
+                                            "\"description\": \"Test Performance Description\", " +
+                                            "\"standardPrice\": \"10.00\", " +
+                                            "\"concessionPrice\": \"9.00\", " +
+                                            "\"boxOffice\": \"Test Box Office\" " +
+                                            "}"
+                            ))
+                    .andExpect(status().isBadRequest());
+
+        }
+
     }
 }
