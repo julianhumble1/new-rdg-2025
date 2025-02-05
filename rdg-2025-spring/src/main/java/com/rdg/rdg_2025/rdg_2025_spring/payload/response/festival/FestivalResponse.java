e0dfbb0f9f27116ffb805fd5dc6214124a9a702e
Julian Humble <humblejulian@gmail.com>
2025-02-05 14:02:20 +0000

    updated festival response to return performances as well

--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/response/festival/FestivalResponse.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/response/festival/FestivalResponse.java
@@ -1,15 +1,21 @@
 package com.rdg.rdg_2025.rdg_2025_spring.payload.response.festival;
 
 import com.rdg.rdg_2025.rdg_2025_spring.models.Festival;
+import com.rdg.rdg_2025.rdg_2025_spring.models.Performance;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter @Setter
 public class FestivalResponse {
 
     private Festival festival;
 
+    private List<Performance> performances;
+
     public FestivalResponse(Festival festival) {
         this.festival = festival;
+        this.performances = festival.getPerformances();
     }
 }
