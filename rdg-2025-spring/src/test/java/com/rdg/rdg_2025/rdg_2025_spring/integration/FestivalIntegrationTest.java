33a1eeb85924bc63ba7c9d99ec47cea19106c993
Julian Humble <humblejulian@gmail.com>
2025-02-07 10:28:01 +0000

    update festival integration test 11 passed

--- rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/integration/FestivalIntegrationTest.java
+++ rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/integration/FestivalIntegrationTest.java
@@ -898,5 +898,26 @@ public class FestivalIntegrationTest {
 
         }
 
+        @Test
+        void testYearBlankResponds400() throws Exception {
+            // Arrange
+            // Act & Assert
+            mockMvc.perform(patch("/festivals/" + existingFestivalId)
+                            .header("Authorization", adminToken)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(
+                                    "{" +
+                                            "\"name\": \"Updated Test Festival\"," +
+                                            " \"venueId\": 2, " +
+                                            "\"year\": \"\", " +
+                                            "\"month\": 2, " +
+                                            "\"description\": \"Updated Test Description\"" +
+                                            "}"
+                            ))
+                    .andExpect(status().isBadRequest());
+
+        }
+
+
     }
 }
