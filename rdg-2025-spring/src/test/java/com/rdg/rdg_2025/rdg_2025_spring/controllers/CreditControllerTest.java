288d0c4557cbe912104e3a154dc9d18869a01353
Julian Humble <humblejulian@gmail.com>
2025-03-12 12:09:36 +0000

    add new credit controller test 5 passed

--- rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/controllers/CreditControllerTest.java
+++ rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/controllers/CreditControllerTest.java
@@ -120,6 +120,17 @@ public class CreditControllerTest {
                     .andExpect(status().isBadRequest());
         }
 
+        @Test
+        void testTypeNotAppropriateResponds400() throws Exception {
+            // Arrange
+            requestJson.put("type", "not actor musician or producer");
+            // Act & Assert
+            mockMvc.perform(post("/credits")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(requestJson)))
+                    .andExpect(status().isBadRequest());
+        }
+
 
     }
 
