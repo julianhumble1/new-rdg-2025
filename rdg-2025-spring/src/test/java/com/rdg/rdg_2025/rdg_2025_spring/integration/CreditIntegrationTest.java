7b5ddb28f6af7230aa180dd4fd68e01f0d1c785e
Julian Humble <humblejulian@gmail.com>
2025-03-12 14:12:19 +0000

    add new credit integration test 10 passed

--- rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/integration/CreditIntegrationTest.java
+++ rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/integration/CreditIntegrationTest.java
@@ -249,6 +249,18 @@ public class CreditIntegrationTest {
                     .andExpect(status().isBadRequest());
         }
 
+        @Test
+        void testProductionIdEmptyResponds400() throws Exception {
+            // Arrange
+            requestJson.put("productionId", "");
+            // Act & Assert
+            mockMvc.perform(post("/credits")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .header("Authorization", adminToken)
+                            .content(objectMapper.writeValueAsString(requestJson)))
+                    .andExpect(status().isBadRequest());
+        }
+
 
     }
 }
