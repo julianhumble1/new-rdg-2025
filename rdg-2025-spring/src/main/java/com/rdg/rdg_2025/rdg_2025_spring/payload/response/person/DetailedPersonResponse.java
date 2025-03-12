06df8931c8411803c6d74bcf1a9e99f6c7105307
Julian Humble <humblejulian@gmail.com>
2025-03-12 18:12:27 +0000

    improved person responses

--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/response/person/DetailedPersonResponse.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/response/person/DetailedPersonResponse.java
@@ -1,8 +1,13 @@
 package com.rdg.rdg_2025.rdg_2025_spring.payload.response.person;
 
 import com.rdg.rdg_2025.rdg_2025_spring.models.Person;
+import com.rdg.rdg_2025.rdg_2025_spring.models.credit.Credit;
+import com.rdg.rdg_2025.rdg_2025_spring.models.credit.CreditType;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter @Setter
 public class DetailedPersonResponse {
@@ -11,7 +16,21 @@ public class DetailedPersonResponse {
 
     private PersonResponseType responseType = PersonResponseType.DETAILED;
 
+    private List<Credit> actingCredits;
+    private List<Credit> musicianCredits;
+    private List<Credit> producerCredits;
+
     public DetailedPersonResponse(Person person) {
         this.person = person;
+
+        this.actingCredits = filterCreditTypes(person.getCredits(), CreditType.ACTOR);
+        this.musicianCredits = filterCreditTypes(person.getCredits(), CreditType.MUSICIAN);
+        this.producerCredits = filterCreditTypes(person.getCredits(), CreditType.PRODUCER);
+    }
+
+    private List<Credit> filterCreditTypes(List<Credit> creditList, CreditType typeToFilter) {
+        return creditList.stream()
+                .filter(credit -> credit.getType() == typeToFilter)
+                .collect(Collectors.toList());
     }
 }
--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/response/production/ProductionResponse.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/payload/response/production/ProductionResponse.java
@@ -2,6 +2,7 @@ package com.rdg.rdg_2025.rdg_2025_spring.payload.response.production;
 
 import com.rdg.rdg_2025.rdg_2025_spring.models.Performance;
 import com.rdg.rdg_2025.rdg_2025_spring.models.Production;
+import com.rdg.rdg_2025.rdg_2025_spring.models.credit.Credit;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,9 +15,12 @@ public class ProductionResponse {
 
     private List<Performance> performances;
 
+    private List<Credit> credits;
+
     public ProductionResponse(Production production) {
         this.production = production;
         this.performances = production.getPerformances();
+        this.credits = production.getCredits();
     }
 
 }
