c21d30c610b4da6a0192a0611586fca64fe78a49
Julian Humble <humblejulian@gmail.com>
2025-03-13 12:27:28 +0000

    update credit controller test 5 passed

--- rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/controllers/CreditControllerTest.java
+++ rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/controllers/CreditControllerTest.java
@@ -444,6 +444,17 @@ public class CreditControllerTest {
                     .andExpect(status().isBadRequest());
         }
 
+        @Test
+        void testTypeMissingResponds400() throws Exception {
+            // Arrange
+            requestJson.remove("type");
+            // Act & Assert
+            mockMvc.perform(patch("/credits/1")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(requestJson)))
+                    .andExpect(status().isBadRequest());
+        }
+
     }
 
 }
