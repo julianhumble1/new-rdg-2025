23404f9b56030d09e95903e3ac5474f86fe560b8
Julian Humble <humblejulian@gmail.com>
2025-03-17 11:07:31 +0000

    update performance controller test 5 passed

--- rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/controllers/PerformanceControllerTest.java
+++ rdg-2025-spring/src/test/java/com/rdg/rdg_2025/rdg_2025_spring/controllers/PerformanceControllerTest.java
@@ -596,6 +596,17 @@ public class PerformanceControllerTest {
                     .andExpect(status().isBadRequest());
         }
 
+        @Test
+        void testVenueIdNotIntResponds400() throws Exception {
+            // Arrange
+            requestJson.put("venueId", "not an int");
+            // Act & Assert
+            mockMvc.perform(patch("/performances/1")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(requestJson)))
+                    .andExpect(status().isBadRequest());
+        }
+
 
 
     }
