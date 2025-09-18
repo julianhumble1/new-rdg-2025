c52d8a1a3a684ad94338e9489e346ebc54fe74f1
Julian Humble <humblejulian@gmail.com>
2025-09-18 11:45:53 +0200

    updated admin dashboard

--- rdg-2025-react/src/components/dashboards/AdminDashboard.jsx
+++ rdg-2025-react/src/components/dashboards/AdminDashboard.jsx
@@ -1,32 +1,64 @@
+import {
+  BuildingLibraryIcon,
+  FilmIcon,
+  ScaleIcon,
+  StarIcon,
+  TicketIcon,
+  TrophyIcon,
+  UserGroupIcon,
+  UsersIcon,
+} from "@heroicons/react/16/solid";
+import ContentCard from "../common/ContentCard.jsx";
 import AdminDashboardCard from "./AdminDashboardCard.jsx";
 
 const AltAdminDashboard = () => {
   return (
-    <div className="w-full bg-slate-200 grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1">
-      <AdminDashboardCard name="Venues" basePath="venues" showSeeAll={true} />
-      <AdminDashboardCard
-        name="Productions"
-        basePath="productions"
-        showSeeAll={true}
-      />
-      <AdminDashboardCard
-        name="Festivals"
-        basePath="festivals"
-        showSeeAll={true}
-      />
-      <AdminDashboardCard
-        name="Performances"
-        basePath="performances"
-        showSeeAll={false}
-      />
-      <AdminDashboardCard name="People" basePath="people" showSeeAll={true} />
-      <AdminDashboardCard
-        name="Credits"
-        basePath="credits"
-        showSeeAll={false}
-      />
-      <AdminDashboardCard name="Awards" basePath="awards" showSeeAll={false} />
-    </div>
+    <ContentCard>
+      <div className="grid sm:grid-cols-3 grid-cols-1">
+        <AdminDashboardCard
+          name="Venues"
+          basePath="venues"
+          showSeeAll={true}
+          icon={<BuildingLibraryIcon />}
+        />
+        <AdminDashboardCard
+          name="Productions"
+          basePath="productions"
+          showSeeAll={true}
+          icon={<ScaleIcon />}
+        />
+        <AdminDashboardCard
+          name="Festivals"
+          basePath="festivals"
+          showSeeAll={true}
+          icon={<StarIcon />}
+        />
+        <AdminDashboardCard
+          name="Performances"
+          basePath="performances"
+          showSeeAll={false}
+          icon={<TicketIcon />}
+        />
+        <AdminDashboardCard
+          name="People"
+          basePath="people"
+          showSeeAll={true}
+          icon={<UsersIcon />}
+        />
+        <AdminDashboardCard
+          name="Credits"
+          basePath="credits"
+          showSeeAll={false}
+          icon={<FilmIcon />}
+        />
+        <AdminDashboardCard
+          name="Awards"
+          basePath="awards"
+          showSeeAll={false}
+          icon={<TrophyIcon />}
+        />
+      </div>
+    </ContentCard>
   );
 };
 
--- rdg-2025-react/src/components/dashboards/AdminDashboardCard.jsx
+++ rdg-2025-react/src/components/dashboards/AdminDashboardCard.jsx
@@ -1,6 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "../common/Button.jsx";
+import { ArrowRightIcon, PlusCircleIcon, PlusIcon } from "@heroicons/react/16/solid";
 
-const AdminDashboardCard = ({ name, basePath, showSeeAll = true }) => {
+const AdminDashboardCard = ({ name, basePath, showSeeAll = true, icon }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="bg-slate-100 hover:bg-slate-200 rounded p-3 m-2 flex">
+      <div className="flex flex-col h-auto min-w-24">
+        <div className="text-sky-900 w-16 h-16 mx-auto">{icon}</div>
+        <div className="mx-auto font-bold">{name}</div>
+      </div>
+      <div className="w-full mx-4 flex flex-col gap-2 justify-center">
+        <Button onClick={() => navigate(`/${basePath}/new`)}>
+          Add New {name !== "People" ? name.slice(0, -1) : "Person"}
+          <PlusCircleIcon className="w-3 h-3 inline ml-2"/>
+        </Button>
+        {showSeeAll && (
+          <Button
+          onClick={() => navigate(`/${basePath}`)}
+          >
+            See All {name}
+          <ArrowRightIcon className="w-3 h-3 inline ml-2" />
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+
   return (
     <div className="hover:bg-slate-300 hover:drop-shadow-sm transition col-span-1 w-full">
       <div className="flex justify-center">
