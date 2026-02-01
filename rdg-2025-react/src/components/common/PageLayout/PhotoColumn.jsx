f77fd5ca40ec8bd21b48e2d1e1ca5dfd14e543b9
Julian Humble <humblejulian@gmail.com>
2026-02-01 12:15:12 +0000

    changed image loading

--- rdg-2025-react/src/components/common/PageLayout/PhotoColumn.jsx
+++ rdg-2025-react/src/components/common/PageLayout/PhotoColumn.jsx
@@ -1,9 +1,28 @@
 import Socials from "../../modals/Socials.jsx";
+import{ useState} from "react"
+import CustomSpinner from "../CustomSpinner.jsx"
 
 const PhotoColumn = ({ imgSrc }) => {
+   const [loaded, setLoaded] = useState(false);
+
   return (
     <div className="flex-1 flex flex-col justify-start">
-      <img src={imgSrc} loading="eager" />
+      {!loaded && (
+        <CustomSpinner />
+      )}
+
+      <img
+        src={imgSrc}
+        alt=""
+        loading="eager"
+        decoding="async"
+        fetchPriority="high"
+        onLoad={() => setLoaded(true)}
+        className={`w-full h-auto transition-opacity duration-300 ${
+          loaded ? "opacity-100" : "opacity-0"
+        }`}
+      />
+
 
       <div className="md:hidden">
         <div className="flex justify-center">Find us on socials:</div>
