d183d18127303a1dda688a226a261ff8a4bb988b
Julian Humble <humblejulian@gmail.com>
2026-04-13 22:08:37 +0100

    image changes

--- rdg-2025-react/src/components/home/HomeProductionSpotLight.jsx
+++ rdg-2025-react/src/components/home/HomeProductionSpotLight.jsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
 import ProductionService from "../../services/ProductionService.js";
-import CloudinaryService from "../../services/CloudinaryService.js";
 import DateHelper from "../../utils/DateHelper.js";
 import { Link } from "react-router-dom";
+import CloudinaryImage from "../common/CloudinaryImage.jsx";
 
 const HomeProductionSpotLight = ({ production }) => {
   const [performanceStatement, setPerformanceStatement] = useState("");
   const [performances, setPerformances] = useState([]);
-  const [flyerUrl, setFlyerUrl] = useState(null);
 
   useEffect(() => {
     const getPerformanceStatement = async () => {
@@ -21,22 +20,12 @@ const HomeProductionSpotLight = ({ production }) => {
     getPerformanceStatement();
   }, [production.id]);
 
-  useEffect(() => {
-    CloudinaryService.getUrl(production.id, "flyers")
-      .then((response) => setFlyerUrl(response.data.url))
-      .catch(() => {});
-  }, [production.id]);
-
   // if (performances.length === 0) return null;
 
   return (
     <div className="flex w-full bg-gray-100  rounded-xl shadow-md hover:shadow-xl transition sm:h-56 max-h-56 relative flex-1">
-      <div className="w-40 shrink-0 rounded-l-xl overflow-hidden">
-        <img
-          className={`w-full h-full ${flyerUrl ? "object-cover" : "object-contain"}`}
-          src={flyerUrl ?? "/images/new_logo_transparent.png"}
-          alt={production.name}
-        />
+      <div className="h-40 w-60 shrink-0 rounded-l-xl overflow-hidden">
+        <CloudinaryImage idNumber={production.id} folder="flyers" fallbackSrc="/images/new_logo_transparent.png" />
       </div>
       <div className="flex flex-col gap-1 p-3 w-1/2 md:w-auto ">
         <div className="flex">
