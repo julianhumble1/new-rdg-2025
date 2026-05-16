f561051921623ffca803e93ac1f61679a7dca671
Julian Humble <humblejulian@gmail.com>
2026-05-16 16:53:49 +0100

    added faster queries

--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/repository/ProductionRepository.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/repository/ProductionRepository.java
@@ -1,6 +1,7 @@
 package com.rdg.rdg_2025.rdg_2025_spring.repository;
 
 import com.rdg.rdg_2025.rdg_2025_spring.models.Production;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,14 +9,23 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductionRepository extends JpaRepository<Production, Integer> {
 
+    @EntityGraph(value = "Production.withAssociations")
+    List<Production> findAll();
+
+    @EntityGraph(value = "Production.withAssociations")
+    Optional<Production> findById(Integer id);
+
+
     @Query(value = "SELECT COUNT(*) FROM productions p WHERE p.name LIKE BINARY :prefix%", nativeQuery = true)
     int countByNameStartingWith(@Param("prefix") String prefix);
 
-    @Query(value = "SELECT p FROM Production p JOIN p.performances perf WHERE perf.time > :currentTime")
+
+    @Query("SELECT DISTINCT p FROM Production p JOIN p.performances perf LEFT JOIN FETCH p.venue WHERE perf.time > :currentTime")
     List<Production> findAllProductionsWithFuturePerformances(@Param("currentTime") LocalDateTime currentTime);
 
 }
--- rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/repository/VenueRepository.java
+++ rdg-2025-spring/src/main/java/com/rdg/rdg_2025/rdg_2025_spring/repository/VenueRepository.java
@@ -1,7 +1,14 @@
 package com.rdg.rdg_2025.rdg_2025_spring.repository;
 
 import com.rdg.rdg_2025.rdg_2025_spring.models.Venue;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VenueRepository extends JpaRepository<Venue, Integer> {
+
+    @EntityGraph(value = "Venue.withAssociations")
+    Optional<Venue> findById(Integer id);
+
 }
